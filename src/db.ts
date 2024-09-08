a8b26c82b89048a500b8f43bd2984d376aa0f3b3
robin <robin.dm@outlook.fr>
2024-09-08 22:07:20 +0200

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,21 +1,35 @@
-import type { Connection } from 'mysql';
-import mysql from 'mysql';
-import type { Matiere } from './types';
+import type { Connection } from "mysql";
+import mysql from "mysql";
+import type { Matiere } from "./types";
 
-export function createDevoirsConnection(host:string, username:string, password:string, dbName:string) {
+export function createDevoirsConnection(
+  host: string,
+  username: string,
+  password: string,
+  dbName: string
+) {
   return mysql.createConnection({
     host: host,
     user: username,
     password: password,
-    database: dbName
+    database: dbName,
   });
 }
 
-export async function addDevoir(connection: Connection, matiere:Matiere, date:string, contenu:string) {
-  try {
-    //connection.execute('INSERT INTO Devoirs(matiere, date_limite, contenu) VALUES (?, ?, ?)', [matiere, date, contenu]);
-    return true;
-  } catch {
-    return false;
-  }
+export function addDevoir(
+  connection: Connection,
+  matiere: Matiere,
+  date: string,
+  contenu: string
+) {
+  return new Promise((resolve, reject) => {
+    connection.commit();
+    connection.query(
+      "INSERT INTO Devoirs(matiere, date_limite, contenu) VALUES (?, ?, ?)",
+      [matiere, date, contenu],
+      (error, results, fields) => {
+        error ? reject(error) : resolve(results);
+      }
+    );
+  });
 }
