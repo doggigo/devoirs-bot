3459f6826801375c7cca7f423c21fb934b675fab
robin <robin.dm@outlook.fr>
2024-09-08 20:59:46 +0200

    Update test_db.ts

--- test/test_db.ts
+++ test/test_db.ts
@@ -4,7 +4,7 @@ const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME } = Bun.env;
 async function main() {
 
   console.log(`${DB_USERNAME}@${DB_HOST} PASSWORD : ${DB_PASSWORD} ON DB '${DB_NAME}'`)
-  return mysql.createConnection({
+  return await mysql.createConnection({
     host: DB_HOST,
     user: DB_USERNAME,
     password: DB_PASSWORD,
