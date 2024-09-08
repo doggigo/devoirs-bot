74625873d170b9147aa34a6b9767395fdcdbe24a
robin <robin.dm@outlook.fr>
2024-09-08 20:55:30 +0200

    Create test_db.ts

---
+++ test/test_db.ts
@@ -0,0 +1,15 @@
+import mysql from 'mysql2/promise'
+
+const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME } = Bun.env;
+async function main() {
+
+  console.log(`${DB_USERNAME}@${DB_HOST} PASSWORD : ${DB_PASSWORD} ON DB '${DB_NAME}'`)
+  return mysql.createConnection({
+    host: DB_HOST,
+    user: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME
+  });
+}
+
+main();
